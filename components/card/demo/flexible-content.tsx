f74cd2abda0efff9a2815ea2e455f3880d5162aa
72757459abhi <93644699+72757459abhi@users.noreply.github.com>
2023-07-01 16:07:03 +0530

    Update flexible-content.tsx
    Signed-off-by: 72757459abhi <93644699+72757459abhi@users.noreply.github.com>

--- components/card/demo/flexible-content.tsx
+++ components/card/demo/flexible-content.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => (
     style={{ width: 240 }}
     cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
   >
-    <Meta title="Europe Street beat" description="www.instagram.com" />
+    <Meta title="Europe Street beat jsjjks" description="www.instagram.com" />
   </Card>
 );
 
